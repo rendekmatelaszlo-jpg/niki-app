13153fb4bccc264c85082067780c86da150a74d2
rendekmatelaszlo-jpg <rendekmatelaszlo@gmail.com>
2026-03-14 13:31:24 +0100

    Create Főtevékenység.kt

---
+++ app/src/main/app/src/main/java/Főtevékenység.kt
@@ -0,0 +1,12 @@
+package com.mate.niki
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Főtevékenység : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(android.R.layout.simple_list_item_1)
+    }
+}
