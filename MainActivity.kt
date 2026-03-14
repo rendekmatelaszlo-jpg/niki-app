0ad2d99cb4e7b21f4cd7032ce7864fee999db12d
rendekmatelaszlo-jpg <rendekmatelaszlo@gmail.com>
2026-03-14 13:19:29 +0100

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,40 @@
+package com.mate.niki
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewFeature
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            WebSettingsCompat.setForceDark(webView.settings, WebSettingsCompat.FORCE_DARK_ON)
+        }
+
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    override fun onBackPressed() {
+        val webView: WebView = findViewById(R.id.webView)
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
--- README.txt
+++ README.txt
@@ -1,17 +1,24 @@
-Niki 4.0 PWA
+NIKI 7.0 Android projekt
 
-Új elemek a 4.0-ban:
-- új jelenet/mód rendszer: normál, fókusz, közeli, játék, nyugodt
-- beszélgetési sorozat (streak)
-- fókusz cél mentése
-- mentés importálása JSON fájlból
-- fejlettebb emlékezet minták
-- gyors fókusz és kedves üzenet gombok
+Ez egy Android Studio projektforrás a NIKI 7.0 prototípushoz.
 
-Feltöltés:
-1. a teljes mappa tartalmát töltsd fel a GitHub repository gyökerébe
-2. Settings > Pages > Deploy from branch > main
-3. Chrome-ban megnyitva telepíthető
+Mit tud jelenleg:
+- nőies, futurisztikus lila dizájn
+- AI avatar aura effekttel
+- hangulatváltás: Nyugodt / Pajzán / Komoly
+- akkumulátorkímélő mód kapcsoló
+- chat felület
+- "NIKI gondolkodik" állapot
+- beszéd animáció és hanghullám effekt
 
-Megjegyzés:
-Ez továbbra is helyi, szabályalapú PWA. Nincs külső AI szerver.
+APK készítés Android Studio-ban:
+1. Csomagold ki a ZIP-et
+2. Nyisd meg a projektet Android Studio-ban
+3. Várd meg, amíg a Gradle szinkron lefut
+4. Menü: Build -> Generate App Bundles or APK -> Build APK
+
+Package név:
+com.mate.niki
+
+Verzió:
+7.0
